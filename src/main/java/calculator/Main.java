5febb8bb89202bb7e00031a1766b0574a8ccd6f6
Tuhin Paul <tuhin@localhost.localdomain>
2018-03-30 12:34:29 -0400

    reading expression from comman line. If worng input is provided, usage example is prompted.

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -12,24 +12,22 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		Calculator calc = new Calculator();
 
-		String[] inputs = {
-			"let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b))",
-			"let(a, 5, let(b, mult(a, 10),add(b, a)))",
-			"let(a, 5, add(a, a))",
-			"mult(add(2, 2), div(9, 3))",
-			"add(1, mult(2, 3))",
-			"add(1, 2)"
-		};
+		if(args.length == 1) {
+			Calculator calc = new Calculator();
 
-		try {
-			for(String expr: inputs) {
+			try {
+				String expr = args[0];
 				System.out.println(calc.evaluate(expr));
 			}
+			catch(BadInputException ex) {
+				System.err.println("Error occurred. Please check stacktrace below:");
+				ex.printStackTrace();
+			}
 		}
-		catch(BadInputException ex) {
-			ex.printStackTrace();
+		else {
+			System.err.println("Only single argument must be provided. Usage example:");
+			System.err.println("calculator.Main \"let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b))\"");
 		}
 	}
 	
