b66e55bd6d6a8496c7037bc0c09b5d3c12b47243
Tuhin Paul <tuhin@localhost.localdomain>
2018-03-31 14:46:39 -0400

    more test cases

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -41,23 +41,69 @@ public class CalculatorTest {
 
 
 
+	/**
+	 * Test with first let operand not being a variable
+	 * */
+	@Test
+	public void testWrongLet1() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.FIRST_LET_OPERAND_SHOULD_BE_VARIABLE);
+		int sum = calc.evaluate("let(add(2,3), 5, 0)");
+	}
+
 
 	/**
-	 *  Empty string should raise Calculator.BadInputException.
-	 */
-	@Test(expected=Calculator.BadInputException.class)
-	public void evaluateEmptyExpression() throws Calculator.BadInputException {
-		System.out.println("Empty string should raise Calculator.BadInputException.");
-		int sum = calc.evaluate("");
+	 * Test with wrong number of operands
+	 * */
+	@Test
+	public void testWrongNumOfOperands1() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		/**
+		 * NOTE: Change TokenTree.makeTree() implementation to report WRONG_NUM_OF_OPERANDS in this case:
+		 * */
+		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+		int sum = calc.evaluate("add(3,4,2)");
+	}
+
+
+	/**
+	 * Test undefined variable
+	 * */
+	@Test
+	public void testUndefinedVar1() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.UNDEFINED_VARIABLE);
+		int sum = calc.evaluate("let(a, add(a,6), 5)");
 	}
 
 	/**
-	 *  string with only spaces should raise Calculator.BadInputException.
-	 */
-	@Test(expected=Calculator.BadInputException.class)
-	public void evaluateWhitespaceExpression() throws Calculator.BadInputException {
-		System.out.println("String with only spaces should raise Calculator.BadInputException.");
-		int sum = calc.evaluate("  	");
+	 * Test undefined variable
+	 * */
+	@Test
+	public void testUndefinedVar2() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.UNDEFINED_VARIABLE);
+		int sum = calc.evaluate("let(a,a,5)");
+	}
+
+	/**
+	 * Test wrong variable position
+	 * */
+	@Test
+	public void testWrongVarPosition1() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.WRONG_VARIABLE_POSITION);
+		int sum = calc.evaluate("b let(a,a,5) c");
+	}
+
+	/**
+	 * Test Malformed expression
+	 * */
+	@Test
+	public void testBadExpr1() throws Calculator.BadInputException {
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+		int sum = calc.evaluate("let(a,a,5) b");
 	}
 
 
@@ -67,28 +113,68 @@ public class CalculatorTest {
 	@Test
 	public void evaluateNull() throws Calculator.BadInputException {
 		System.out.println("Evaluating null should raise Calculator.BadInputException.");
+
 		thrown.expect(Calculator.BadInputException.class);
 		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+
 		int sum = calc.evaluate(null);
 	}
+
+
+	/**
+	 *  Empty string should raise Calculator.BadInputException.
+	 */
+	@Test
+	public void evaluateEmptyExpression() throws Calculator.BadInputException {
+		System.out.println("Empty string should raise Calculator.BadInputException.");
+
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+
+		int sum = calc.evaluate("");
+	}
+
+	/**
+	 *  string with only spaces should raise Calculator.BadInputException.
+	 */
+	@Test
+	public void evaluateWhitespaceExpression() throws Calculator.BadInputException {
+		System.out.println("String with only spaces should raise Calculator.BadInputException.");
+
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+
+		int sum = calc.evaluate("  	");
+	}
+
 
 	/**
 	 *  string with only delimiters should raise Calculator.BadInputException.
 	 */
-	@Test(expected=Calculator.BadInputException.class)
+	@Test
 	public void evaluateOnlyDelimiters() throws Calculator.BadInputException {
 		System.out.println("String with only delimiters should raise Calculator.BadInputException.");
+
+		thrown.expect(Calculator.BadInputException.class);
+		thrown.expectMessage(Calculator.BAD_EXPRESSION);
+
 		int sum = calc.evaluate(",() ");
 	}
+
+
+
+
 
 	/**
 	 *  string with non-alphanumeric characters should raise Calculator.BadInputException.
 	 */
 	@Test
 	public void evaluateNonAlphaNumeric() throws Calculator.BadInputException {
+		System.out.println("String with non-alphanumeric characters should raise Calculator.BadInputException.");
+
 		thrown.expect(Calculator.BadInputException.class);
 		thrown.expectMessage(Calculator.UNKNOWN_LITERAL_TYPE);
-		System.out.println("String with non-alphanumeric characters should raise Calculator.BadInputException.");
+
 		int sum = calc.evaluate("[!");
 	}
 
--- src/test/java/calculator/MainTest.java
+++ src/test/java/calculator/MainTest.java
@@ -53,20 +53,25 @@ public class MainTest extends TestCase
 		testExpressions.put("add(1, 2)", 3);
 		testExpressions.put("let(Let, 5, add(Let, Let))", 10);
 
+		testExpressions.put("let(a,5,a)", 5);
+		testExpressions.put("let(a, add(4,6), 5)", 5);
+
 		String[] expressions = {
-			"let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b))",
-			"let(a, 5, let(b, mult(a, 10),add(b, a)))",
-			"let(a, 5, add(a, a))",
-			"mult(add(2, 2), div(9, 3))",
-			"add(1, mult(2, 3))",
-			"add(1, 2)",
-			"let(Let, 5, add(Let, Let))"
+				"let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b))",
+				"let(a, 5, let(b, mult(a, 10),add(b, a)))",
+				"let(a, 5, add(a, a))",
+				"mult(add(2, 2), div(9, 3))",
+				"add(1, mult(2, 3))",
+				"add(1, 2)",
+				"let(Let, 5, add(Let, Let))",
+				"let(a, add(4,6), 5)",
+				"let(a,5,a)"
 		};
 
 
 		for (String expr: expressions) {
 
-			System.out.print("Testing " + expr);
+			System.out.println("Testing " + expr);
 
 			try {
 				int foundVal = calc.evaluate(expr);
@@ -103,7 +108,7 @@ public class MainTest extends TestCase
 
 		for (String expr: expressions) {
 
-			System.out.print("Testing " + expr);
+			System.out.println("Testing " + expr);
 
 			try {
 				int foundVal = calc.evaluate(expr);
